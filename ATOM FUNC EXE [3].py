22232a9f1333491d95049c54ea9a99759bef276b
Andrean2305 <andreanhasan@gmail.com>
2021-10-26 13:21:57 +0700

    Revsion func Exercise

--- ATOM FUNC EXE [3].py
+++ ATOM FUNC EXE [3].py
@@ -9,3 +9,12 @@ x = num_atom(10, 12.001)
 y = num_atom(10, 1.008)
 print("\n")
 print(z,x,y)
+#if you want to input by yourself
+
+baaaaaaz = num_atom(a = eval(input("Weight in gram: ")),b = eval(input("Atomic weight in grams/mole: ")))
+zze = eval(input("Weight in gram: "))
+zzee = eval(input("Atomic weight in grams/mole[Type None to get default Aurum atomic weight]: "))
+if zzee == None:
+    zzzz = num_atom(zze)
+else:
+    zzzz = num_atom(zze,zzee)
--- CALC PLANET FUNC EXE[2].py
+++ CALC PLANET FUNC EXE[2].py
@@ -1,9 +1,19 @@
 def calc_weight_on_planet(a,b=23.1): #a = weight // b = gravity
     c = a/9.8 #c = mass
-    print(f"THE WEIGHT = {a}, THE GRAVITY = {b}")
-    print("Your weight in this planet = ",c * b)
+    print(f"Your weight on earth = {a}, The gravity of the planet = {b}")
+    print("Your weight in other planet = ",c * b)
     print("\n")
 
 z = calc_weight_on_planet(120,9.8)
 x = calc_weight_on_planet(120)
 y = calc_weight_on_planet(120,23.1)
+#If you want to input with your number
+zze = eval(input("Your weight: "))
+zzee = eval(input("The planet gravity(Type None to get default 23.1 m/s^2 gravity): "))
+if zzee == None:
+    zzzz = calc_weight_on_planet(zze)
+else:
+    zzzz = calc_weight_on_planet(zze,zzee)
+    #and then zzzzzzz = calc_weight_on_planet(zze,zzee)
+#or
+zeeeeee = calc_weight_on_planet(a=eval(input("Your weight: ")),b=eval(input("The planet gravity: ")))
