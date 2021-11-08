a111d48c0f81bd207bfd34f77e238ac9a13b1277
Andrean2305 <andreanhasan@gmail.com>
2021-11-08 12:00:03 +0700

    Number1 File Assignment

---
+++ FILE ASSIGNMENT NUMBER 1.py
@@ -0,0 +1,25 @@
+import os
+os.chdir('C:/Users/Asus')
+file1 = open("66684-0.txt","r") 
+s = file1.read()
+cc = s.lower()
+dd = cc.replace(",",'')
+gg = dd.replace(".",'')
+hh = gg.replace("(",'')
+ii = hh.replace(")",'')
+
+def zeee(a):
+    b = {}
+    for c in a.split():          #Untuk menghitung banyaknya suatu kata
+        b[c] = b.get(c , 0)+1
+    return b
+
+ze = zeee(ii)
+print(ze)
+
+baba = list(ze)
+
+for i in baba :
+    ccc = (ze[i])
+    if ccc == 1 :
+        print(f"This is the hapax : {i} {ccc}")
