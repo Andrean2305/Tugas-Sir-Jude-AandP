ed2fc941f9330d0cb0b57f94ba2bbe94e4d87b96
Andrean2305 <andreanhasan@gmail.com>
2021-11-08 12:29:11 +0700

    Number 1 Revision File Assignment

--- FILE ASSIGNMENT NUMBER 1.py
+++ FILE ASSIGNMENT NUMBER 1.py
@@ -7,22 +7,23 @@ dd = cc.replace(",",'')
 gg = dd.replace(".",'')
 hh = gg.replace("(",'')
 ii = hh.replace(")",'')
-
 def zeee(a):
     b = {}
     for c in a.split():          #Untuk menghitung banyaknya suatu kata
         b[c] = b.get(c , 0)+1
     return b
 
-def pro (a) :
-    for i in a :
-    ccc = (ze[i])
-    if ccc == 1 :
-     print(f"This is the hapax : {i} {ccc}")
-     return (i)
-
 ze = zeee(ii)
+
 baba = list(ze)
 
-azzzz = pro(baba)
-print(azzzz)
+def pro(a) :
+    for i in a :
+        ccc = (ze[i])
+        if ccc == 1 :
+         print(f"This is the hapax : {i} {ccc}")
+    return i
+    
+
+aaa = pro(baba)
+#finally work sir im happy
